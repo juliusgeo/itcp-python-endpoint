8899c6b63a2afcc9653d713f5baafc2f39240f21
Julius Park <juliusgeo@gmail.com>
2020-05-22 17:31:18 -0400

    added prettier output from GAP

--- endpoint/endpoint.py
+++ endpoint/endpoint.py
@@ -2,8 +2,8 @@ import sys
 sys.path.append('../')
 from gap_python.gap_python import ITCP
 t = ITCP()
-out = t.__execfunction__("b:=[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ];")
-print(out)
+out = t.__execfunction__("rlist:=NCRateRegionOB([[[ [ 1, 2 ], [ 1, 2, 4 ] ], [ [ 2, 3 ], [ 2, 3, 5 ] ],[ [ 4, 5 ], [ 4, 5, 6 ] ], [ [ 3, 4 ], [ 3, 4, 7 ] ],[ [ 1, 6 ], [ 3, 1, 6 ] ], [ [ 6, 7 ], [ 2, 6, 7 ] ],[ [ 5, 7 ], [ 1, 5, 7 ] ] ], 3, 7 ],true,[]);;")
+print()
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
@@ -22,7 +22,8 @@ def executefunction(func, data):
 	if func.lower() == "netsymgroup":
 		return t.__execfunction__("G:=NetSymGroup("+str(data)+");")
 	elif func.lower() == "ncrateregionob":
-		return t.__execfunction__("rlist1:=NCRateRegionOB2("+str(data)+",true,[]);;Display(rlist1[2]);")
+		t.__execfunction__("rlist1:=NCRateRegionOB2("+str(data)+",true,[]);;Display(rlist1[2]);")
+		return t.__execfunction__("Display(rlist[2]);")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
